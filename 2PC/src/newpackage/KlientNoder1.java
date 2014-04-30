faa1ddfcff864dd17030815165dd558c29ce9db2
Aruynify <sindre.aroen@yahoo.com>
2014-04-30 12:49:45 +0200

    only yes nodes

--- 2PC/src/newpackage/KlientNoder1.java
+++ 2PC/src/newpackage/KlientNoder1.java
@@ -42,7 +42,7 @@ public class KlientNoder1 {
                     System.out.println("Jeg er klar, og gir respons til koordinator...");
                     Socket connection = new Socket("localhost", 1250);
                     PrintWriter writer = new PrintWriter(connection.getOutputStream(), true);
-                    writer.println("no");
+                    writer.println("yes");
                     System.out.println("Jeg forbereder meg til utførelse...");
                     fw = new FileWriter(fileName, true);
                     bw = new BufferedWriter(fw);
